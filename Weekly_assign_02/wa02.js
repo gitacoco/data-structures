e6a22de8e2d1204c317a132b1ce1cd95c916516f
Zac Zhibang Jiang <zhibang.jiang@gmail.com>
2020-09-17 00:26:53 +0800

    Update wa02.js

--- Weekly_assign_02/wa02.js
+++ Weekly_assign_02/wa02.js
@@ -3,20 +3,17 @@
 var fs = require('fs');
 var cheerio = require('cheerio');
 
-// load the 4.txt file into a variable, `content`
-// this is the file that we created in the starter code from last week
-var content = fs.readFileSync('data/4.txt');
+var content = fs.readFileSync('data/4.txt');                    // load the 4.txt file into a variable, `content`
+var $ = cheerio.load(content);                                  // load `content` into a cheerio object
+var addressList = '';                                           
 
-// load `content` into a cheerio object
-var $ = cheerio.load(content);
-
-// var addressList = ''; // this variable will hold the lines of text
-var addressList = '';
-
-//Use CSS selector to select all td with tyle="border-bottom:1px solid #e3e3e3; width:260px"
-$('td[style="border-bottom:1px solid #e3e3e3; width:260px"] b').each(function(i, elem){
-  addressList += $(elem.nextSibling.nextSibling).text().split(',')[0].split('-')[0].split('Rm')[0].trim() + '\n'
+$('td[style="border-bottom:1px solid #e3e3e3; width:260px"] b') // Use CSS selector to select all td with a specific tyle and element
+    .each(function(i, elem){
+    addressList += $(elem.nextSibling.nextSibling).text()       // Use .nextSibling method to re-navigate to a new object
+                                                .split(',')[0]  // Split it and only keep the first object
+                                                .split('-')[0]  // Split it and only keep the first object
+                                                .split('Rm')[0] // Split it and only keep the first object
+                                                .trim() + '\n'  // Remove spaces and make the variable hold the lines
 });
    
-//write the addresses to a text file
-fs.writeFileSync('data/addressList.txt', addressList);
+fs.writeFileSync('data/addressList.txt', addressList);          // Write all the addresses to a text file
